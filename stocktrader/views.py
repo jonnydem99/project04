f2abf20a267a2ac60ae5b0d7d46796c376db0f67
Jonathan Niess <niessj@gmail.com>
2020-10-10 16:26:49 -0500

    connected API

--- stocktrader/views.py
+++ stocktrader/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+
 def home( request): 
-    return render( request, 'stocktrader/home.html')
+    import requests
+    import json
+
+    #pk_f7c3d0d74a06499fbf6aa2d949922047
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_f7c3d0d74a06499fbf6aa2d949922047")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render( request, 'stocktrader/home.html', {'api': api})
 
 def about( request): 
     return render( request, 'stocktrader/about.html')
