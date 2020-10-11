bea90f21d7a275d29a15a6b62702bcd1687440ca
Jonathan Niess <niessj@gmail.com>
2020-10-11 12:21:22 -0500

    small alignment fixes in Bootstrap

--- stocktrader/views.py
+++ stocktrader/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 
 
-def home( request): 
+def home(request):
     import requests
     import json
 
-    #pk_f7c3d0d74a06499fbf6aa2d949922047
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_f7c3d0d74a06499fbf6aa2d949922047")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_f7c3d0d74a06499fbf6aa2d949922047")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'stocktrader/home.html', {'api': api})
 
-    return render( request, 'stocktrader/home.html', {'api': api})
+    else:
+        return render(request, 'stocktrader/home.html', {'ticker': "Enter a ticker symbol above."})
 
-def about( request): 
-    return render( request, 'stocktrader/about.html')
+
+def about(request):
+    return render(request, 'stocktrader/about.html')
